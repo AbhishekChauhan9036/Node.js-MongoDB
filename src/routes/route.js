c6f5d84a615858ee3b1d3664e997c728d39bea1c
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-12-09 08:02:20 +0530

    Assignment Solutions nodejs-modules

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const intro = require('./introduction')
+const wel=require('../logger/logger.js')
+const ddmmyy=require('../util/helper.js')
 const employee = require('./employee')
 const _ = require('underscore')
 
 router.get('/test-me', function (req, res) {
-    console.log("email from introduction module", intro.myEmail)
-    intro.myFunction('Sabiha')
-    console.log("email from employee module", employee.myEmail)
+    // console.log("email from introduction module", intro.myEmail)
+    // intro.myFunction('Sabiha')
+    // console.log("email from employee module", employee.myEmail)
 
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    let result = _.first(days, 4)
-    console.log(`Result from underscore function is ${result}`)
+    // const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    // let result = _.first(days, 4)
+    // console.log(`Result from underscore function is ${result}`)
+/*Assignment 1 Call.............................................................................*/
+    wel.welcome("Abhishek Chauhan");
+
+/*Assignment 2 Call.............................................................................*/
+    ddmmyy.printDate();
+    ddmmyy.printMonth();
+    ddmmyy.getBatchInfo();
 
 
     res.send('any dummy text')
