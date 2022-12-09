9b409f0d1b777f0ecbc33d3cc687822fff476d70
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-12-09 09:47:07 +0530

    Assignment Solutions nodejs-modules

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const fp=require('lodash')
 const intro = require('./introduction')
 const wel=require('../logger/logger.js')
 const ddmmyy=require('../util/helper.js')
@@ -8,13 +9,8 @@ const employee = require('./employee')
 const _ = require('underscore')
 
 router.get('/test-me', function (req, res) {
-    // console.log("email from introduction module", intro.myEmail)
-    // intro.myFunction('Sabiha')
-    // console.log("email from employee module", employee.myEmail)
 
-    // const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    // let result = _.first(days, 4)
-    // console.log(`Result from underscore function is ${result}`)
+
 /*Assignment 1 Call.............................................................................*/
     wel.welcome("Abhishek Chauhan");
 
@@ -27,6 +23,25 @@ router.get('/test-me', function (req, res) {
     opestr.trimMethod();
     opestr.changetoLowerCase();
     opestr.changeToUpperCase();
+
+
+/*Assignment 4 Call.............................................................................*/
+    const days = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let result = _.first(days, 4)
+    console.log(result)
+
+    const oddNumber=[1,3,5,7,9,11,13,15,17,19];
+    let ansoodNumber=_.tail(oddNumber);
+    console.log(ansoodNumber);
+
+    const array1=[1, 2, 9, 40],array2=[1, 20, 3, 2];
+    const withoutDublicate= _.union( array1, array2);
+    console.log(withoutDublicate);
+
+
+    let pairarr=[["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]];
+    let obj = fp.fromPairs(pairarr);
+    console.log(obj)
 
 
     res.send('any dummy text')
