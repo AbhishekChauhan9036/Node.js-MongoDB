c0f1a935f3b46d3cdff9db85e848004569c5be49
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-12-12 21:29:35 +0530

    assignment/missing number

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,5 @@
-// Video Link:- https://drive.google.com/file/d/1WqFuDn6vz-hTyYvTezx3rRqX6EFbpLW1/view?usp=sharing 
-
-
+// Video Solution Link 1:- https://drive.google.com/file/d/15HBju70OHVfsfbhL9wbomGpzVO3Nefu8/view?usp=share_link
+// Video Solution Link 2:- https://drive.google.com/file/d/1IlZwnMDLUdNsTiagBzWK80hJk9M1zE0V/view?usp=sharing
 
 const express = require("express");
 const router = express.Router();
@@ -8,51 +7,39 @@ const intro = require("./introduction");
 const employee = require("./employee");
 const _ = require("underscore");
 
+
 // Assignment 1.................................................................
-router.get('/movies', function (req, res) {
-    const webSeries =  ["Mirjapur", "Hostel Daze", "Girls Hostel", "Kota Factory", "Mission MBBS", "Aspirants"];
-    res.send(webSeries);
+router.get("/sol1", function (req, res) {
+  let arr = [1, 2, 3, 5, 6];
+  let sum1 = 0;
+  let sum2 = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum1 += arr[i];
+  }
+  let n = arr[arr.length - 1];
+  sum2 = (n * (n + 1)) / 2;
+  let ans = sum2 - sum1;
+  res.send(`${ans}`);
 });
 
 
 // Assignment 2.................................................................
-router.get('/movies/:indexNumber',function(req,res){
-    const webSeries =  ["Mirjapur", "Hostel Daze", "Girls Hostel", "Kota Factory", "Mission MBBS", "Aspirants"];
-        if(webSeries.length>=req.params.indexNumber){
-            res.send(webSeries[req.params.indexNumber]);
-        }
-});
+router.get("/sol2", function (req, res) {
+  let arr = [33, 34, 35, 37, 38];
+  let sum1 = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum1 += arr[i];
+  }
 
+  let sum2 = 0;
+  let st = arr[0];
+  let en = arr[arr.length - 1];
+  for (let i = st; i <= en; i++) {
+    sum2 += i;
+  }
 
-// Assignment 3.................................................................
-router.get('/movies/:indexNumber',function(req,res){
-    const webSeries =  ["Mirjapur", "Hostel Daze", "Girls Hostel", "Kota Factory", "Mission MBBS", "Aspirants"];
-        if(webSeries.length<=req.params.indexNumber){
-            res.send("Please Enter a valid index Number");
-        }
-});
-
-
-// Assignment 4.................................................................
-router.get("/films", function (req, res) {
-  const arr = ["The Shining","Incendies","Rang de Basanti","Finding Nemo"];
-  const obj = Object.assign({}, arr)
-  res.send(obj);
-});
-
-
-// Assignment 5.................................................................
-router.get('/films/:id',function(req,res){
-        let obj={
-            "id":3,
-            "name":"Rang de Basanti"
-        }
-
-        if(obj.id==req.params.id){
-            res.send(obj.name);
-        }
-
-        res.send("No movie exists with this id");
+  let ans = sum2 - sum1;
+  res.send(`${ans}`);
 });
 
 module.exports = router;
