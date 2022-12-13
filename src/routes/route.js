07effaeb67d3cad393189e789db178fa2ac1fba6
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-12-13 21:43:07 +0530

    post api assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,23 +1,43 @@
 const express = require("express");
 const router = express.Router();
 
+let players = [
+  {
+    name: "manish",
+    dob: "1/1/1995",
+    gender: "male",
+    city: "jalandhar",
+    sports: ["swimming"],
+  },
+  {
+    name: "gopal",
+    dob: "1/09/1995",
+    gender: "male",
+    city: "delhi",
+    sports: ["soccer"],
+  },
+  {
+    name: "lokesh",
+    dob: "1/1/1990",
+    gender: "male",
+    city: "mumbai",
+    sports: ["soccer"],
+  },
+];
 
-router.post("/test-post-3", function (req, res) {
-  let id = req.body.user;
-  let pwd = req.body.password;
+router.post("/players", function (req, res) {
+  let pname = req.body.name;
+  let counter = 0;
+  for (let i = 0; i < players.length; i++) {
+    if (players[i]["name"] == pname) {
+      counter = 1;
+    }
+  }
 
-  console.log(id, pwd);
-
-  console.log(req.body);
-
-  res.send({ msg: "hi", status: true });
-});
-
-router.post("/test-post-4", function (req, res) {
-  let arr = [12, "functionup"];
-  let ele = req.body.element;
-  arr.push(ele);
-  res.send({ msg: arr, status: true });
+  if (counter == 0) {
+    players.push(req.body);
+  }
+  res.send({ data: players, status: true });
 });
 
 module.exports = router;
