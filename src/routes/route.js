19b981fa6d68d7e684af934c11adf3424c87e0d3
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-12-16 08:55:04 +0530

    MongoDB Intro & Code Day 1

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,44 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const UserModel= require("../models/userModel.js")
+const UserController= require("../controllers/userController")
 
-let persons = [
-  {
-    name: "PK",
-    age: 10,
-    votingStatus: false,
-  },
-  {
-    name: "SK",
-    age: 20,
-    votingStatus: false,
-  },
-  {
-    name: "AA",
-    age: 70,
-    votingStatus: false,
-  },
-  {
-    name: "SC",
-    age: 5,
-    votingStatus: false,
-  },
-  {
-    name: "HO",
-    age: 40,
-    votingStatus: false,
-  },
-];
+router.get("/test-me", function (req, res) {
+    res.send("My first ever api!")
+})
 
-router.post("/query-params", function (req, res) {
-  let input = req.query.input;
-  let arr = [];
-  for (i = 0; i < persons.length; i++) {
-    if (persons[i].age > input) {
-      persons[i].votingStatus = true;
-      arr.push(persons[i]);
-    }
-  }
-  return res.send(arr);
-});
+router.post("/createUser", UserController.createUser  )
+
+router.get("/getUsersData", UserController.getUsersData)
 
 module.exports = router;
