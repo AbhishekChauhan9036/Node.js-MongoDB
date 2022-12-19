043d2dd8bc5e764ca7182ff8a41d7f4be737fbe9
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-12-19 07:20:11 +0530

    create a book collection in your DB

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// mongoose.connect("mongodb+srv://Tapaswee2001Samantaray:jecky2001@cluster0.zinufff.mongodb.net/TapasweeDB1-mongoDBDayFirstPractice", {
-//     useNewUrlParser: true
-// } , mongoose.set("strictQuery" , true))
-mongoose.connect("mongodb+srv://abhishek9036:abhishek9036@abhishek9036.s0p4ddl.mongodb.net/abhishek9036-DB", {
+mongoose.connect("mongodb+srv://Tapaswee2001Samantaray:jecky2001@cluster0.zinufff.mongodb.net/TapasweeDB1-mongoDBDayFirstPractice", {
     useNewUrlParser: true
 } , mongoose.set("strictQuery" , true))
+// mongoose.connect("mongodb+srv://abhishek9036:abhishek9036@abhishek9036.s0p4ddl.mongodb.net/abhishek9036-DB", {
+//     useNewUrlParser: true
+// } , mongoose.set("strictQuery" , true))
 // mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
 //     useNewUrlParser: true
 // })
