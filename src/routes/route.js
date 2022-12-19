85ae5ac699882f972974a77ff30db433d3e36783
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-12-19 09:52:46 +0530

    create a book collection in your DB

--- src/routes/route.js
+++ src/routes/route.js
@@ -14,6 +14,17 @@ router.get("/getUsersData", UserController.getUsersData)
 
 router.post("/createBook", BookController.createBook  )
 
+router.get("/bookList",BookController.bookList)
+
+router.get("/getBooksInYear",BookController.getBooksInYear)
+
+router.get("/getParticularBooks",BookController.getParticularBooks)
+
+router.get("/getXINRBooks",BookController.getXINRBooks)
+
+router.get("/getRandomBooks",BookController.getRandomBooks)
+
 router.get("/getBooksData", BookController.getBooksData)
+
 
 module.exports = router;
