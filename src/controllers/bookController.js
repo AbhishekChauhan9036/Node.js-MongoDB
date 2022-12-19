216bf0bd3cd8f35b1e21c14aa654f5e414372695
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-12-19 18:34:44 +0530

    create a book collection in your DB

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -10,33 +10,35 @@ const createBook = async function (req, res) {
   res.send({ msg: savedData });
 };
 
-
 const getBooksData = async function (req, res) {
   // 2...........................................................................
-  let data=await bookModel.find();
-  let data=await bookModel.find().select({bookName:1,authorname:1,_id:0});
-
+  // let ans = await bookModel.find();
+  let ans = await bookModel
+    .find()
+    .select({ bookName: 1, authorname: 1, _id: 0 });
 
   // 3............................................................................
-  let {year}=req.query;
+  /*let { year } = req.query;
   console.log(year);
-  let data=await bookModel.find({year:{eq:year}});
+  let ans = await bookModel.find({ year: { eq: year } }); */
 
-
-  // 4............................................................................ 
-  let bodyData=req.body;
-  let data=await bookModel.find(bodyData);
-
+  // 4............................................................................
+  /* let bodyData = req.body;
+  let ans = await bookModel.find(bodyData); */
 
   // 5............................................................................
-  let data=await bookModel.find({$or:[{"price.indianPrice":{$eq:"Rs 3000"}},{"price.indianPrice":{$eq:"Rs 2000"}},
-   {"price.indianPrice":{$eq:"Rs 4000"}}]}) 
+  /* let ans = await bookModel.find({
+    $or: [
+      { "price.indianPrice": { $eq: "Rs 3000" } },
+      { "price.indianPrice": { $eq: "Rs 2000" } },
+      { "price.indianPrice": { $eq: "Rs 4000" } },
+    ],
+  });*/
 
+  //6.............................................................................
+  /*  let ans=await bookModel.find({$or:[{stockAvailable:{$eq:true}},{totalPages:{$gt:3000}}]}); */
 
-   //6.............................................................................
-   let data=await bookModel.find({$or:[{stockAvailable:{$eq:true}},{totalPages:{$gt:3000}}]});
-
-   res.send({result:data});
+  res.send({ result: ans });
 };
 
 module.exports.createBook = createBook;
